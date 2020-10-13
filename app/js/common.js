2e1adc17731bc442aa47229352f210c60f371977
viktoriazaharova <zaharova181114@gmail.com>
2020-10-13 18:40:16 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -3,3 +3,11 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
+
+$('.btn-burger').click(function () {
+    $('.menu-mobile').fadeToggle();
+});
+
+$('.btn-close').click(function () {
+    $('.menu-mobile').fadeOut();
+});
